5661749e010bb6876dbfb272655abc3b9c1a3965
Osakana16 <fish.pokelab@yandex.com>
2021-02-22 19:04:28 +0900

    ワールドを読み込む処理を追加。
    その他、地形毎の画像を選択する為のPaletteクラスを追加。

--- src/KAction_Editor.py
+++ src/KAction_Editor.py
@@ -1,5 +1,6 @@
 # KAction-Editor.py - 本プログラムのスタートアップ部
-import tkinter, tkinter.messagebox
+import tkinter, tkinter.messagebox, tkinter.filedialog
+import worldeditor
 
 # プログラム自身
 class Program(tkinter.Tk):
@@ -9,15 +10,29 @@ class Program(tkinter.Tk):
         # ウィンドウ設定
         self.title("KAction-Editor")
         self.geometry("640x480")
+        self.palette = worldeditor.Palette(self)
+
+        # リストボックスの設定
+        # 読みこまれたワールド一覧
+        self.world_list = tkinter.Listbox(master=self, selectmode='multiple')
+        # ダブルクリックされたワールドを読みこむ。
+        self.world_list.bind('<Double-Button-1>', lambda e: self.palette.Load(worldeditor.Loader(self.world_list.get('active').split('.')[0])))
+        self.world_list.grid()
 
         # メニューの設定
         self.menubar = tkinter.Menu(self)
+
         # ファイル関連
         self.filemenu = tkinter.Menu(self.menubar, tearoff=0)
         self.filemenu.add_command(label='新規作成')
-        self.filemenu.add_command(label='開く')
-        self.filemenu.add_command(label='終了', command=self.OnQuit)
+        self.filemenu.add_command(label='開く', command=self.OpenWorld)
+        self.filemenu.add_command(label='終了', command=self.destroy)
         self.menubar.add_cascade(label='ファイル', menu=self.filemenu)
+
+        # ツール
+        self.toolmenu = tkinter.Menu(self.menubar, tearoff=0)
+        self.toolmenu.add_command(label='パレット', command=self.palette.deiconify)
+        self.menubar.add_cascade(label='ツール', menu=self.toolmenu)
 
         # ヘルプ関連
         import webbrowser
@@ -33,9 +48,19 @@ class Program(tkinter.Tk):
     def Run(self):
         self.mainloop()
 
+    # マップを開く
+    def OpenWorld(self):
+        import os, glob
+        folder_name = tkinter.filedialog.askdirectory(initialdir=os.path.abspath(os.path.dirname(__file__)))
+        # フォルダーからファイル一覧を取得
+        files = glob.glob(folder_name + '/*.pmap')
+        for file in files:
+            self.world_list.insert(0, file)
+
     # 終了処理
-    def OnQuit(self):
-        quit()
+    def destroy(self):
+        self.palette.quit()
+        super().destroy()
 
 program = Program()
 program.Run()
---
+++ src/worldeditor.py
@@ -0,0 +1,58 @@
+import tkinter, tkinter.messagebox
+
+# ワールド読込器
+class Loader:
+    def __init__(self, path: str):
+        self.chips = dict()
+        self.elems = list()
+
+        # マップの読み込み
+        with open(path + '.pmap', mode='r', encoding='utf_16') as f:
+            for line in f.readlines():
+                self.elems.append([])
+                target = self.elems[-1]
+                for c in line:
+                    target.append(c)
+                target.pop(-1)
+
+        # csvの読み込み
+        import csv
+        with open(path + '.csv', mode='r', newline='', encoding='utf_16') as csvfile:
+            cells = csv.reader(csvfile)
+            for row in cells:
+                self.chips[row[0]] = row[1]
+
+        for key in self.chips.keys():
+            # 最初のセルはスキップ
+            if key == '要素':
+                continue
+            elif len(key) != 1:
+                raise ValueError
+            self.chips[key] = tkinter.PhotoImage(file=self.chips[key])
+
+# パレットウィンドウ
+class Palette(tkinter.Toplevel):
+    def __init__(self, master=None):
+        super().__init__(master=master)
+        self.title('パレット')
+        self.transient(master)
+        self.__chips = dict()
+        self.__elems = list()
+        self.__canvas = tkinter.Canvas(self)
+        self.__canvas.place(x=0, y=0)
+
+    def destroy(self):
+        self.withdraw()
+
+    def Load(self, loader: Loader):
+        self.__chips = loader.chips
+        self.__elems = loader.elems
+
+        i = 1
+        for key in self.__chips.keys():
+            # 最初のセルを無視。
+            if key == '要素':
+                continue
+
+            self.__canvas.create_image(i * 60, 60, image=self.__chips[key])
+            i += 1
