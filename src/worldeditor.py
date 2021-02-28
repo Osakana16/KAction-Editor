2bbab1780287606b0f0f3a0d1480f57e34ddd16f
Osakana16 <fish.pokelab@yandex.com>
2021-02-28 20:43:45 +0900

    マップを編集するための処理を追加。
    パレットからチップを選んだ状態でcanvasをクリックすると、そのチップを設置する処理を追加。

--- src/worldeditor.py
+++ src/worldeditor.py
@@ -44,7 +44,7 @@ class Palette(tkinter.Toplevel):
         self.__elems = list()
         self.__canvas = tkinter.Canvas(self)
         self.__canvas.grid()
-        self.__selecting_elem = None
+        self.__selecting_elem = ' '
 
     def destroy(self):
         self.withdraw()
@@ -64,8 +64,15 @@ class Palette(tkinter.Toplevel):
                 continue
 
             self.__canvas.create_image(i * 60 + 60, 60, image=chips[key])
-            self.__elems[i].append(chips[key])
+            self.__elems[i].append(key)
             i += 1
+
+    def __Select(self, event: tkinter.Event):
+        x, y = (event.x // 120, event.y // 120)
+        try:
+            self.__selecting_elem = self.__elems[y][x]
+        except:
+            print(self.__selecting_elem)
 
 class MapCanvas(tkinter.Canvas):
     def __init__(self, w, h, frame: tkinter.Frame, palette: Palette):
@@ -79,11 +86,12 @@ class MapCanvas(tkinter.Canvas):
         self.__scroll_bars[1].grid(row=0, column=1, sticky=tkinter.S + tkinter.N)
         self.config(xscrollcommand=self.__scroll_bars[0].set, yscrollcommand=self.__scroll_bars[1].set)
 
-        self.bind('<ButtonPress-1>')
+        self.bind('<ButtonPress-1>', self.__Pressed)
         self.bind('<Motion>', self.__Draw)
         self.__elems = list()
         self.__chips = dict()
         self.__palette = palette
+        self.__width, self.__height = (0, 0)
 
     # loaderから構造を参照する。
     def Load(self, loader):
@@ -97,17 +105,49 @@ class MapCanvas(tkinter.Canvas):
             if temp > mx:
                 mx = temp
 
-        self.config(scrollregion=(0, -60, mx * 120, my * 120))
+        self.__width, self.__height = (mx * 120, my * 120)
+        self.config(scrollregion=(0, -60, self.__width, self.__height))
+
+    def __PutChip(self, x, y, ch):
+        if y < 0 or x < 0:
+            return
+        
+        must_loop = True
+        while must_loop:
+            try:
+                self.__elems[y][x] = ch
+                must_loop = False
+            except IndexError:
+                if len(self.__elems) <= y:
+                    self.__elems.append([])
+                elif len(self.__elems[y]) <= x:
+                    self.__elems[y].append(' ')
+
+    def __Pressed(self, event: tkinter.Event):
+        print(self.winfo_width(), self.winfo_height())
+        if not self.__HasDrawingElems():
+            return
+
+        mx = ((int(self.__scroll_bars[0].get()[1] * self.__width) - self.winfo_width()) + event.x)
+        my = ((int(self.__scroll_bars[1].get()[1] * self.__height) - self.winfo_height()) + event.y)
+        x, y = (mx // 120, my // 120)
+        self.__PutChip(x, y, self.__palette.SelectingElem())
 
     def __Draw(self, event: tkinter.Event):
+        self.create_rectangle(0, -60, self.__width, self.__height*2, fill='black')
         if not self.__HasDrawingElems():
             return
 
         for y in range(len(self.__elems)):
-            for x in range(len(self.__elems[y])):
-                elem = self.__elems[y][x]
-                if elem != ' ':
-                    self.create_image(x * 120, y * 120, image=self.__chips[elem])
+            for x in range(self.__width // 120):
+                try:
+                    elem = self.__elems[y][x]
+                    if elem != ' ':
+                        self.create_image(x * 120, y * 120, image=self.__chips[elem])
+                except IndexError:
+                    self.create_rectangle(x * 120 - 60, y * 120 - 60, x * 120 + 120 - 60, y * 120 + 120  - 60, fill='red')
+                except KeyError:
+                    print(elem)
 
     def __HasDrawingElems(self) -> bool:
         return len(self.__elems) > 0
